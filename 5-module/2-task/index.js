d917bafaaf6931af157a404d2343127095d5c594
javascript-ru <47786167+jsru@users.noreply.github.com>
2020-06-08 16:28:58 +0200

    Added solution for task: Компонент Сортируемая таблица (SortableTable)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -15,15 +15,57 @@
  */
 function SortableTable(items) {
   /**
-   * @property {Element} - обязательное свойство, которое ссылается на элемент <table>
+   * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
   this.el = document.createElement('table');
 
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+
+  thead.innerHTML = `
+         <tr>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Salary</td>
+            <td>City</td>
+         </tr>
+    `;
+
+  this.el.appendChild(thead);
+  this.el.appendChild(tbody);
+
+  function render() {
+    tbody.innerHTML = items.map((item) => {
+      let row = '';
+
+      for (const key in item) {
+        row += `<td>${item[key]}</td>`;
+      }
+
+      return `<tr>${row}</tr>`;
+    }).join('');
+  }
+
   /**
    * Метод выполняет сортировку таблицы
-   * @param {number} column - номер колонки, по которой
-   * нужно выполнить сортировку (отсчет начинается от 0)
+   * @param {number} column - номер колонки, по которой нужно
+   *  выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    items = items.sort((a, b) => {
+      const keyA = Object.keys(a)[column];
+      const keyB = Object.keys(b)[column];
+
+      if (desc) {
+        return a[keyA] < b[keyB] ? 1 : -1;
+      }
+
+      return a[keyA] > b[keyB] ? 1 : -1;
+    });
+
+    render();
+  };
+
+  render();
 }
